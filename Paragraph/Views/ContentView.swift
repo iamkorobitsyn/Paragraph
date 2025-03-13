22ce6170fe475a015485a996b248dba1ba011a12
Александр Коробицын <iamkorobitsyn@icloud.com>
2025-03-13 21:26:08 +0300

    13.03

--- Paragraph/Views/ContentView.swift
+++ Paragraph/Views/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                 ZStack {
                     Image("mainTexture")
                         .resizable()
+                    
                     Color(.orange).opacity(0.1)
                     HStack(spacing: 0) {
                         ToolBar(sizeWidth: device == .pad ? 560 : 360,
