eb868d87596fd1ac306417cea198a417b458d7ed
Александр Коробицын <iamkorobitsyn@icloud.com>
2025-04-02 16:24:02 +0300

    02.04

--- Paragraph/Components/PageView.swift
+++ Paragraph/Components/PageView.swift
@@ -10,39 +10,54 @@ struct BookReader: View {
     @State private var pageOffset: CGFloat = 0
     let pages = [Color.green, Color.blue, .clear]
     
+    let textLines: [TextLine]
+    
     var body: some View {
-        TabView(selection: $selectedPage) {
-            ForEach(pages.indices, id: \.self) { index in
-                GeometryReader { geometry in
-                    pages[index]
-                        .overlay(
-                            VStack {
-                                Text("Страница \(index + 1)")
-                                Text("Смещение: \(Int(geometry.frame(in: .global).minX))")
+        ZStack {
+            Text("Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test")
+                .opacity(calculateOpacity()) // opacity растёт при прокрутке
+            
+            TabView(selection: $selectedPage) {
+                ForEach(pages.indices, id: \.self) { index in
+                    GeometryReader { geometry in
+                        pages[index]
+                            .overlay(
+                                VStack {
+                                    Text("Страница \(index + 1)")
+                                    Text("Смещение: \(Int(geometry.frame(in: .global).minX))")
+                                }
+                            )
+                            .onChange(of: geometry.frame(in: .global).minX) { oldValue, newValue in
+                                pageOffset = newValue
+                                print("Страница \(index): смещение \(newValue)")
                             }
-                        )
-                        .onChange(of: geometry.frame(in: .global).minX) { oldValue, newValue in
-                            pageOffset = newValue
-                            print("Страница \(index): смещение \(newValue)")
-                        }
-                }
-                .onAppear { print("Страница \(index) стала видимой") }
-                .onDisappear {
-                    print("Страница \(index) скрылась")
-                    selectedPage = 1
+                    }
+                    .onAppear { print("Страница \(index) стала видимой") }
+                    .onDisappear {
+                        print("Страница \(index) скрылась")
+                        selectedPage = 1
+                    }
                 }
             }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .overlay(
+                Text("Текущее смещение: \(Int(pageOffset))")
+                    .padding()
+                    .background(.white)
+                    .padding(.top, 50)
+            )
         }
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .overlay(
-            Text("Текущее смещение: \(Int(pageOffset))")
-                .padding()
-                .background(.white)
-                .padding(.top, 50)
-        )
+    }
+    
+    private func calculateOpacity() -> Double {
+        let screenWidth = UIScreen.main.bounds.width
+        let normalizedOffset = abs(pageOffset / screenWidth)
+        
+        // opacity увеличивается от 0 до 1 при прокрутке
+        return min(1, normalizedOffset)
     }
 }
 
 #Preview {
-    BookReader()
+    BookReader(textLines: [TextLine([Word(id: 0, text: "kjfh")], .annotation, 34, false, false)])
 }
--- Paragraph/Views/Reader.swift
+++ Paragraph/Views/Reader.swift
@@ -76,20 +76,18 @@ struct ReaderView: View {
                     Color(backgroundColor)
                         .ignoresSafeArea()
 
-                    HStack() {
-                        Text("Цирк семьи пайло")
-                            .foregroundStyle(Color.gray)
-                            .lineLimit(1)
-                            .multilineTextAlignment(.center)
-                            .padding([.leading, .trailing], padding)
-                            .padding(.top, 20)
-                    }
-   
-                    VStack() {
+                    VStack(spacing: 0) {
                         
                         HStack() {
+                            Text("Цирк семьи пайло")
+                                .foregroundStyle(Color.gray)
+                                .lineLimit(1)
+                                .multilineTextAlignment(.center)
+                                .padding([.leading, .trailing], 40)
+                                .padding(.bottom, 9)
                             
                             Spacer()
+                              
                             Selector(mode: .readerControls) { i in
                                 if i == 0 {
                                     settingsPresented.toggle()
@@ -99,12 +97,16 @@ struct ReaderView: View {
                                 }
                             }
                             .padding(.trailing, 20)
+                            .padding([.top, .bottom], 9)
                         }
-                        .frame(height: 50)
+                        .frame(height: 60)
+                      
+                        
+                        
                         
                         ZStack {
                             
-                            TabView(selection: $currentPage) {
+                        
                                 LazyVStack(spacing: 0) {
                                     ForEach(0..<textLines.count, id: \.self) { index in
                                         TextLineView(font: font,
@@ -116,89 +118,21 @@ struct ReaderView: View {
                                     }
                                 }
                                 
-                                Rectangle().fill(.clear)
-                            }
-                            .tabViewStyle(.page)
-                            .onChange(of: currentPage) {
-                                pages.append(2)
-                                pages.removeFirst()
-                            }
                             
-                            TabView(selection: $currentPage) {
-                                LazyVStack(spacing: 0) {
-                                    ForEach(0..<textLines.count, id: \.self) { index in
-                                        TextLineView(font: font,
-                                                     fontColor: textColor,
-                                                     textLine: textLines[index],
-                                                     interval: interval,
-                                                     padding: padding,
-                                                     endBlock: textLines[index].isEndOfBlock)
-                                    }
-                                }
-                                
-                                Rectangle().fill(.clear)
-                            }
-                            .tabViewStyle(.page)
-                            .onChange(of: currentPage) {
-                                pages.append(2)
-                                pages.removeFirst()
-                            }
+                         
                             
                         }
+                        .frame(height: geometry.size.height - 100)
+                 
                         
-                        
-                        
-                        
-                        
-                        
-                        
-//                        GeometryReader { geometry in
-//                            ScrollView(.horizontal, showsIndicators: true) {
-//                                        HStack(spacing: 0) {
-//                                            ForEach(0..<pages.count, id: \.self) { page in
-//                                                LazyVStack(spacing: 0) {
-//                                                    ForEach(0..<textLines.count, id: \.self) { index in
-//                                                        TextLineView(font: font,
-//                                                                     fontColor: textColor,
-//                                                                     textLine: textLines[index],
-//                                                                     interval: interval,
-//                                                                     padding: padding,
-//                                                                     endBlock: textLines[index].isEndOfBlock)
-//                                                    }
-//                                                }
-//                                                .tag(page)
-//                                            }
-//                                        }
-//                                        .offset(x: -CGFloat(currentPage) * geometry.size.width) // Перемещение страниц
-//                                        .animation(.linear, value: currentPage) // Анимация перелистывания
-//                                    }
-//                            .scrollDisabled(true)
-//                                    .gesture(
-//                                        DragGesture()
-//                                            .onEnded { value in
-//                                                let threshold = geometry.size.width / 3
-//                                                if value.translation.width < -threshold {
-//                                                    currentPage = min(currentPage + 1, pages.count - 1)
-//                                                } else if value.translation.width > threshold {
-//                                                    currentPage = max(currentPage - 1, 0)
-//                                                }
-//                                            }
-//                                    )
-//                                }
-                        
-                        
-                        
-                        Spacer()
-                    }
                     
-                    VStack {
-                        Spacer()
                         Text("58 %")
                             .foregroundStyle(Color.gray)
-                        .frame(height: 50)
-                        .padding(.bottom, 10)
+                            .frame(height: 40)
+                            .padding(.top, 10)
                     }
-                    .ignoresSafeArea()
+                    
+                    
                 }
                 VStack {
                     Spacer()
